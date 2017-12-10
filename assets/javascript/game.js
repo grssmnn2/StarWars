52576c19fa9d550049a069617bd6a423644ed733
Cecily Grossmann <grssmnn2@gmail.com>
2017-12-10 14:10:11 -0600

    added buttons with images and background image

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+// declare variables
+var userChoice;
+
+// create click actions for each character button/image
+
+$("#luke").on("click", function(){
+
+
+});
+
+$("#leia").on("click", function(){
+
+
+});
+
+$("#jarjar").on("click", function(){
+
+
+});
+
+
+$("#vader").on("click", function(){
+
+
+});
+
+
+// relate points per game/click
+
+// write win/lose function to run depending on outcome
+
+// each image should move around when clicked/change background color based on game role
+
+});
