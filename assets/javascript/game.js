6ef1d9988cb75608cc59e1a92d138dd2d9a49f58
Cecily Grossmann <grssmnn2@gmail.com>
2017-12-10 21:45:23 -0600

    added button color change and battle button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,31 +6,51 @@ var userChoice;
 // create click actions for each character button/image
 
 $("#luke").on("click", function(){
-
+    // $("#luke").fadeToggle(1000);
+    $("#luke").css({
+        background: 'green', 
+        float: 'right'
+    });
 
 });
 
 $("#leia").on("click", function(){
-
+    // $("#leia").fadeToggle(1000);
+    $("#leia").css({
+        background: 'green',
+        float: 'right'
+    });
 
 });
 
 $("#jarjar").on("click", function(){
-
+    // $("#jarjar").fadeToggle(1000);
+    $("#jarjar").css({
+        background: 'red',
+        float: 'right'
+    });
 
 });
 
 
 $("#vader").on("click", function(){
-
-
+    // $("#vader").fadeToggle(1000);
+    $("#vader").css({
+        background: 'red',
+        float: 'right'
+    });
 });
 
+// first click is user hero, second click is enemy to battle
+
+// when clicked, user hero moves above other characters to new row, background color becomes green
+
+// when clicked, enemy moves below other characters to new row, background color becomes red
 
 // relate points per game/click
 
 // write win/lose function to run depending on outcome
 
-// each image should move around when clicked/change background color based on game role
+
 
 });
